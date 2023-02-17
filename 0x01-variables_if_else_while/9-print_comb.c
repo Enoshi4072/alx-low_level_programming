cb1184ee41929e90229db0d548ddc831eab4969b
Enock Kibet <enockkibet6642@gmail.com>
2023-02-17 17:00:14 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+for (j = a; b <= 9; b++)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a != 9 || b != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
