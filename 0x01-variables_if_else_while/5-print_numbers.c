3e707a9ca235ea49167b2048ff34a8cdbbc12f3f
Enock Kibet <enockkibet6642@gmail.com>
2023-02-17 15:57:00 +0000

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base to starting from 0.
+ * Return: Always 0
+ */
+int main(void)
+{
+int number = 0;
+while (number < 10)
+{
+printf("%d", number);
+number++;
+}
+printf("\n");
+return (0);
+}
