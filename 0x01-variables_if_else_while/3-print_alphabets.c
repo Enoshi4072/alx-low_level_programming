9a34195691416964a526551a3b060edec56bf874
Enock Kibet <enockkibet6642@gmail.com>
2023-02-17 15:36:35 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+char l = 'a' :
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+l = 'A';
+while (l <= 'Z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
