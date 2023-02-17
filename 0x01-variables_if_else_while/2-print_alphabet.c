fefae275d270b647b2da16cb07b1f0239e2187e6
Enock Kibet <enockkibet6642@gmail.com>
2023-02-17 15:20:05 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet characters in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+charlowe_case = 'a';
+while (lower_case <= 'z')
+{
+	putchar(lower_case);
+	lower_case++;
+}
+putchar('\n');
+return (0);
+}
