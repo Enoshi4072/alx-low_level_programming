f5f86f89528009978291a1cf5923295b589475fc
Enock Kibet <enockkibet6642@gmail.com>
2023-02-21 09:27:10 +0000

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,9 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-if (c >= 'a' && c >= 'z')
-{
-	return (1);
-}
-return (0);
+	return (c >= 'a' && c <= 'z');
 }
