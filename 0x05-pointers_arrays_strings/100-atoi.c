e25e84eeab271aaa45d51e8a80ae0c1e776e291c
Enock Kibet <enockkibet6642@gmail.com>
2023-02-27 21:02:07 +0000

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,45 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
+ * _atoi - The function converts a string to an integer
+ * @s: string to be converted *
  * Return: the int converted from the string
  */
-#include <stdio.h>
-
 int _atoi(char *s)
 {
-int  auth = 1;
-int number = 0;
-int number2;
+	int num1, num2, num3, length, size, number;
 
-while (*s)
-{
-if (*s == '-')
-auth *= -1;
-else if (*s >= '0' && *s <= '9')
-{
-number2 = *s - '0';
-number = number * 10 + number2;
-}
-else if (number > 0)
-break;
-s++;
-}
+	num1 = 0;
+	num2 = 0;
+	num3 = 0;
+	length = 0;
+	size = 0;
+	number = 0;
 
-return (number *auth);
+	while (s[length] != '\0')
+		length++;
+
+	while (num1 < length && size == 0)
+	{
+		if (s[num1] == '-')
+			++num2;
+
+		if (s[num1] >= '0' && s[num1] <= '9')
+		{
+			number = s[num1] - '0';
+			if (num2 % 2)
+				number = -number;
+			num3 = num3 * 10 + number;
+			size = 1;
+			if (s[num1 + 1] < '0' || s[num1 + 1] > '9')
+				break;
+			size = 0;
+		}
+		num1++;
+	}
+
+	if (size == 0)
+		return (0);
+
+	return (num3);
 }
