23e7f48eb49ee8d386af362515fc5b48b54c1069
Enock Kibet <enockkibet6642@gmail.com>
2023-03-03 09:31:16 +0000

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @str: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *rot13(char *str)
+{
+char *p = str;
+while (*p != '\0')
+{
+if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+{
+*p += 13;
+}
+else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+{
+*p -= 13;
+}
+p++;
+}
+return (str);
+}
