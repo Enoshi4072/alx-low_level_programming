a355d12f8664e38cd9eeb539b543c59d4bb7aec4
Enock Kibet <enockkibet6642@gmail.com>
2023-03-08 15:38:37 +0000

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,52 +1,30 @@
+#include "main.h"
+
 /**
- * helperfunc - The function utilizes a binary search to find a natural
- * square root of a number.
- * @low: The lower boundary of the integer range to search for.
- * @high: The upper boundary of the integer range to search for.
- * @n: The number whose range is to be calculated.
- * Return: The resulting square root.
+ * base_recursion - recursively finds the natural
+ * square root of a number
+ * @n: number to whose squareroot is calculated
+ * @i: iterator
+ * Return: the resulting square root
  */
-int helperfunc(int n, int low, int high)
+int base_recursion(int n, int i)
 {
-int center = (low + high) / 2;
-int square = center * center;
-
-if (low > high)
-{
+if (i * i > n)
 return (-1);
-}
-if (square == n)
-{
-return (center);
-}
-else if (square < n)
-{
-return (helperfunc(n, center + 1, high));
-}
-else
-{
-return (helperfunc(n, low, center - 1));
-}
+if (i * i == n)
+return (i);
+return (base_recursion(n, i + 1));
 }
 
 /**
- * _sqrt_recursion - The function returns the natural square root of a number.
- * @n: The number to calculate the square root for.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: The resulting square root.
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
 return (-1);
-}
-else if (n == 0 || n == 1)
-{
-return (n);
-}
-else
-{
-return (helperfunc(n, 0, n / 2));
-}
+return (base_recursion(n, 0));
 }
