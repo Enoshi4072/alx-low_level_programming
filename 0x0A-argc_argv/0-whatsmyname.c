7f7cb7e877d1e3c596504d9acaf33a5bed80be05
Enock Kibet <enockkibet6642@gmail.com>
2023-03-11 16:28:21 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (int argc, char* argv[]) {
-_putchar(argv[0]);
-return 0;
+/**
+ * main - prints the name of the program
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ * Return: Always 0(Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
 }
