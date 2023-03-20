97e938087adf1a80cb4906c0dba71de23a4a69f1
Enock Kibet <enockkibet6642@gmail.com>
2023-03-20 20:43:53 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+
+#define DOG
+/**
+ *struct dog - short description of the dog structure
+ *@name: - First member which is a pointer to the dog
+ *@age: - Second member which is the age of the dog
+ *@owner: - Third member which is a pointer to the owner of the dog
+ *Description: Contains the data of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
