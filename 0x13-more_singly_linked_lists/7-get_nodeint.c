117fcc48b496632f6e0937e58b7265131ccc4769
Enoshi4072 <enockkibet6642@gmail.com>
2023-04-03 20:33:16 +0000

    7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,5 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	nodeptr = head;
 	for (i = 0; i < index; i++)
 		nodeptr = nodeptr->next;
+	if (nodeptr == NULL)
+		return (NULL);
 	return (nodeptr);
 }
