95ce119311448f7136a4b9a75be1318ab00d2431
Enoshi4072 <enockkibet6642@gmail.com>
2023-04-03 18:47:47 +0000

    4. Free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a the memory address of listint_t
+ * @head: The list to be freed
+ * Return: NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr_node;
+	while (*head != NULL)
+	{
+		ptr_node = (*head)->next;
+		free(*head);
+		*head = ptr_node;
+	}
+	*head = (NULL);
+}
