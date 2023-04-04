5743c8efcd66895cff7256f0c8e21e2e4f8114ab
Enoshi4072 <enockkibet6642@gmail.com>
2023-04-04 09:47:15 +0000

    13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,22 +8,22 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *ptr_current, *holder_ptr;
-    size_t total = 0;
+	listint_t *ptr_current, *holder_ptr;
+	size_t total = 0;
 
-    if (h == NULL)
-        return (total);
+	if (h == NULL)
+		return (total);
 
-    ptr_current = *h;
-    while (ptr_current != NULL)
-    {
-        total++;
-        holder_ptr = ptr_current;
-        ptr_current = ptr_current->next;
-        free(holder_ptr);
-        if (holder_ptr <= ptr_current)
-            break;
-    }
-    *h = NULL;
-    return (total);
+	ptr_current = *h;
+	while (ptr_current != NULL)
+	{
+	total++;
+	holder_ptr = ptr_current;
+	ptr_current = ptr_current->next;
+	free(holder_ptr);
+	if (holder_ptr <= ptr_current)
+		break;
+	}
+	*h = NULL;
+	return (total);
 }
