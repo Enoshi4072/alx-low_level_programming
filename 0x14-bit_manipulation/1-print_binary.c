a99743e2f683ee7e2bf6092ca890204118306179
Enoshi4072 <enockkibet6642@gmail.com>
2023-04-06 14:54:27 +0000

    Write a function that prints the binary representation of a number.
    Prototype: void print_binary(unsigned long int n);
    Format: see example
    You are not allowed to use arrays
    You are not allowed to use malloc
    You are not allowed to use the % or / operators

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_binary - Prints the bibary representation of a number
+ * @n: The number whose binary representation is printed
+ */
+void print_binary(unsigned long int n)
+{
+	int i, counter = 0;
+	int val_default = 1;
+
+	if (n == 0)
+	{
+	_putchar('0');
+	return;
+	}
+
+	for (i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--)
+	{
+	if ((n >> i) & val_default)
+	{
+	_putchar('1');
+		counter++;
+	}
+	else if (counter)
+	{
+	_putchar('0');
+	}
+	}
+}
