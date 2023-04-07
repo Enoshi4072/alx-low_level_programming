ccf7cfde38176914259b2428be84dd67b08edd7e
Enoshi4072 <enockkibet6642@gmail.com>
2023-04-07 13:06:19 +0000

    Write a function that checks the endianness.
    Prototype: int get_endianness(void);
    Returns: 0 if big endian, 1 if little endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *ptr_to_endian = (char *)&i;
+	char val_endian = *ptr_to_endian;
+
+	return ((val_endian == 1) ? 1 : 0);
+}
